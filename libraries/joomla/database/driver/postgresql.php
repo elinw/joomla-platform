a36b0892e4bdb29c4c744d85aa9c8987471c0898
elinw <elin.waring@gmail.com>
2013-01-03 19:15:43 -0500

    fix

--- libraries/joomla/database/driver/postgresql.php
+++ libraries/joomla/database/driver/postgresql.php
@@ -1306,7 +1306,7 @@ class JDatabaseDriverPostgresql extends JDatabaseDriver
 	 * Updates a row in a table based on an object's properties.
 	 *
 	 * @param   string   $table    The name of the database table to update.
-	 * @param   object   $object  A reference to an object whose public properties match the table fields.
+	 * @param   object   $object   An object whose public properties match the table fields.
 	 * @param   string   $key      The name of the primary key.
 	 * @param   boolean  $nulls    True to update null fields or false to ignore them.
 	 *
